7c7ad07cc096d06419a93b33c4a45af3a8a7f52d
Zayed Hasan <95176649+potato53@users.noreply.github.com>
2022-10-19 13:03:17 +0600

    Progress

---
+++ Thikana web/LogIN.php
@@ -0,0 +1,109 @@
+<?php
+    require 'config.php';
+    session_start();
+    if(isset($_POST['login'])){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $sql="SELECT * FROM userstry WHERE email='$email'";
+        $result=mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result)>0){
+            $row=mysqli_fetch_array($result);
+            if((password_verify($password, $row['password']))&&($row['email']==$email)){
+                $_SESSION["email"]=$row['email'];
+                $_SESSION["name"]=$row['username'];
+                $_SESSION["phn_no"]=$row['phone_no'];
+                echo  
+                "<script> alert('login  done'); </script> ";
+                header("location: home.php");
+            }else{
+                echo  
+                "<script> alert('wrong password'); </script> ";
+            }
+
+        }else{
+            echo  
+            "<script> alert('Wrong Email'); </script> ";
+        }
+
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Iconscout CSS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Login & Registration Form</title>
+</head>
+<body>
+    <div class="container">
+        <div class="forms">
+            <div class="form login">
+                <span class="title">Login</span>
+
+                <form action="#" method="post">
+                    <div class="input-field">
+                        <input type="text" name="email" id="email" placeholder="Enter your email" required>
+                        <i class="uil uil-envelope icon"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="password" class="password" name="password" placeholder="Enter your password" required>
+                        <i class="uil uil-lock icon"></i>
+                        <i class="uil uil-eye-slash showHidepw"></i>
+                    </div>
+
+                    <div class="chechbox-text">
+                        <div class="checkbox-content">
+                            <input type="checkbox" id="logCheck">
+                            <label for="logCheck" class="text">Remember me</label>
+                        </div>
+
+                        <a href="#" class="text">Forgot password?</a>
+                    </div>
+
+                    <div class="input-field button">
+                        <input type="submit" name="login" value="Login Now">
+                    </div>
+                </form>
+
+                <div class="login-signup">
+                    <span class="text">Not a member?
+                        <a href="registration.php" class="text signup-link">Signup now</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="script.js"></script>
+</body>
+</html>
+
+
+
+
+<!-- <!Doctype html>
+<html>
+    <head>
+        <title>THIKANA</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+    </head>
+    <body>
+        <h2>Log In</h2>
+        <form class="" action="" method="post" >
+            <label for="email">Email: </label>
+            <input type="Text" name="email" id="email" value=""><br>
+            <label for="password">Password: </label>
+            <input type="password" name="password" value=""><br>
+            <input type="submit" name="login" value="LOG IN">
+        </form>
+    </body>
+</html> -->
---
+++ Thikana web/registration.php
@@ -0,0 +1,159 @@
+<?php
+require 'config.php';
+if(isset($_POST['submit'])){
+    $username=$_POST['name'];
+    $email=$_POST['email'];
+    $phn=$_POST['phone_no'];
+    $nidno=$_POST['nid'];
+    $address=$_POST['address'];
+    $password=$_POST['password'];
+    $chkpass=$_POST['check_pass'];
+
+    $select="SELECT * FROM userstry WHERE email='$email'";
+    $result=mysqli_query($conn, $select);
+    if(mysqli_num_rows($result)>0){
+        echo 
+             "<script> alert('email has already taken'); </script> ";
+    }
+    else{
+        if($chkpass==$password){
+            $hashpassword=password_hash($password, PASSWORD_DEFAULT);
+            $insert= "INSERT INTO userstry (username, email, phone_no, nid_no, address, password) VALUES ('$username', '$email', '$phn', '$nidno', '$address', '$hashpassword')";
+            $sql=mysqli_query($conn, $insert);
+            // $stml->bind_param("ssssss", $name, $email, $phn, $nidno, $address, $password);
+            //$stml->execute();
+            //mysqli_query($conn, $query);
+        if($sql){
+                echo 
+                "<script> alert('registration  done'); window.location.href='LogIN.php'; </script> ";
+             }else{
+                 echo 
+                 "<script> alert('registration  failed'); </script> ";
+            }
+
+/*
+        if($sql){
+            echo  
+            "<script> alert('registration  done'); </script> ";
+        }else{
+            echo 
+            "<script> alert('registration  failed'); </script> ";
+        }
+        }else{
+            echo
+            "<script> alert('pass didnt match'); </script> ";
+        }
+*/
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Iconscout CSS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Registration Form</title>
+</head>
+<body>
+    <div class="container">
+        <div class="forms">
+
+
+            <div class="form signup">
+                <span class="title">Registration</span>
+
+                <form action="#" method="post">
+                    <div class="input-field">
+                        <input type="text"  name="name" placeholder="Enter your name" required>
+                        <i class="uil uil-user"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="text"  name="nid" id="nid" placeholder="Enter your nid" required>
+                        <i class="uil uil-user-circle"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="text"  name="email" id="email" placeholder="Enter your email" required>
+                        <i class="uil uil-envelope icon"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="text"  name="phone_no" id="phone_no" placeholder="Enter your phone no." required>
+                        <i class="uil uil-phone"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="address"  name="address" id="address" placeholder="Enter your address" required>
+                        <i class="uil uil-map-marker"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="password" class="password"  name="password" placeholder="Create a password" required>
+                        <i class="uil uil-lock icon"></i>
+                    </div>
+                    <div class="input-field">
+                        <input type="password"  name="check_pass" id="check_pass" class="password" placeholder="Confirm password" required>
+                        <i class="uil uil-lock icon"></i>
+                        <i class="uil uil-eye-slash showHidepw"></i>
+                    </div>
+
+                    <!-- <div class="chechbox-text">
+                        <div class="checkbox-content">
+                            <input type="checkbox" id="logCheck">
+                            <label for="logCheck" class="text">Remember me</label>
+                        </div>
+
+                        <a href="#" class="text">Forgot password?</a>
+                    </div> -->
+
+                    <div class="input-field button">
+                        <input type="submit"   name="submit" value="Signup Now">
+                    </div>
+                </form>
+
+                <div class="login-signup">
+                    <span class="text">Already have an account?
+                        <a href="LogIN.php" class="text login-link">Log in</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+
+</body>
+</html>
+
+<!-- <!Doctype html>
+<html>
+    <head>
+        <title>THIKANA</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+    </head>
+    <body>
+        <h2>Registration</h2>
+        <form class="" action="" method="post" >
+            <label for="name">Name: </label>
+            <input type="Text" name="name" value=""><br>
+            <label for="email">Email: </label>
+            <input type="Text" name="email" id="email" value=""><br>
+            <label for="phone_no">Phone No: </label>
+            <input type="TEXT" name="phone_no" id="phone_no" value=""><br>
+            <label for="nid">NID: </label>
+            <input type="text" name="nid" id="nid" value=""><br>
+            <label for="address">Address: </label>
+            <input type="text" name="address" id="address" value=""><br>
+            <label for="password">Password: </label>
+            <input type="password" name="password" value=""><br>
+            <label for="check_pass">Confirm Password: </label>
+            <input type="password" name="check_pass" id="check_pass" value=""><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </body>
+</html> -->
